6856166fa47722af80a3a9bb213a617869340f38
raahimdevsinc <raahim.ahmad@devsinc.com>
2024-08-23 02:00:49 +0500

    Add rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_094531) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "keywords"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_240_822_094_531) do
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.string 'keywords'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
