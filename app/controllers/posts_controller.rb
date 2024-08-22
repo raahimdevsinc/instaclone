5952f31b5dd9cf033f07a151c8648ba751458651
raahimdevsinc <raahim.ahmad@devsinc.com>
2024-08-22 21:02:44 +0500

    CRUD Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    return nil
   end
 
   # GET /posts/new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+    validates :keywords, presence: true, length: { minimum: 1, maximum: 100 }
 end
+  
