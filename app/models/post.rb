c28823984b4333c85478f8daf73e2792986a0dd5
raahimdevsinc <raahim.ahmad@devsinc.com>
2024-08-23 15:49:50 +0500

    Add Images To Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :title, presence: true, length: { minimum: 1, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 1, maximum: 500 }
   validates :keywords, presence: true, length: { minimum: 1, maximum: 100 }
+  has_many_attached :images
 end
