d192b31cf71cb370c244db13bd03ae443e1a26f5
raahimdevsinc <raahim.ahmad@devsinc.com>
2024-08-25 22:55:59 +0500

    Add user Authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@
 
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /posts or /posts.json
   def index
@@ -66,6 +67,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:title, :description, :keywords, images: [])
+    params.require(:post).permit(:title, :description, :keywords, :user_id, images: [])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
+
+  before_create :randomize_id
+
+  private
+
+  def randomize_id
+    loop do
+      self.id = SecureRandom.random_number(1_000_000_000)
+      break unless User.where(id: id).exists?
+    end
+  end
 end
