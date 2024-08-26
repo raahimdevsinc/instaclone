e0b2dfca138d1566745a87250b9899ffacdc5d1b
raahimdevsinc <raahim.ahmad@devsinc.com>
2024-08-26 14:09:34 +0500

    Revert "Authentication"

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 # config/importmap.rb
 
-pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'bootstrap', to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
